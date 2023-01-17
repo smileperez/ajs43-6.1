ec870ca0d024cce3538a4d6116c1ae05602f10bb
smileperez <aa.efimenko@yandex.ru>
2023-01-17 21:52:10 +0300

    fix first task

--- src/js/backup.orderByProps.js
+++ src/js/backup.orderByProps.js
@@ -2,16 +2,13 @@ export default function orderByProps(obj, pattern) {
   // Сортируем объект, получаем сортированный массив
   const sortedArr = Object.keys(obj).sort();
   // Объединяем pattern с полученным сортированным массивом
-  const concatedArr = pattern.concat(sortedArr);
-
-  const uniqueSet = [];
+  const conactedArr = pattern.concat(sortedArr);
+  // Создаем новый коллекцию уникальных значений сортированного массива
+  const newSet = new Set(conactedArr);
+  // переводим коллекцию в массив
+  const uniqueSet = Array.from(newSet);
+  // рузультирующий массив
   const result = [];
-
-  for (const prop in concatedArr) {
-    if (!uniqueSet.includes(concatedArr[prop])) {
-      uniqueSet.push(concatedArr[prop]);
-    }
-  }
 
   for (let i = 0; i < uniqueSet.length; i += 1) {
     for (const prop in obj) {
